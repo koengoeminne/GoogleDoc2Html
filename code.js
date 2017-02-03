9c4af810ab851f573053f9a7f5533c5d35cc5bfe
koengoeminne <koengoeminne@gmail.com>
2017-02-03 13:32:45 +0100

    Links, text processing & logging to file
    Added logging function
    Cleaned up code here & there (WIP)
    Reworked text processing function
    Made links actually work

--- code.js
+++ code.js
@@ -1,19 +1,25 @@
 function ConvertGoogleDocToCleanHtml() {
   var body = DocumentApp.getActiveDocument().getBody();
-  var numChildren = body.getNumChildren();
   var output = [];
   var images = [];
   var listCounters = {};
 
   // Walk through all the child elements of the body.
-  for (var i = 0; i < numChildren; i++) {
+  for (var i = 0; i < body.getNumChildren(); i++) {
     var child = body.getChild(i);
     output.push(processItem(child, listCounters, images));
   }
 
-  var html = output.join('\r');
+  var html = output.join('\r\n');
   emailHtml(html, images);
-  //createDocumentForHtml(html, images);
+  sendLog();
+}
+
+function sendLog() {
+  var fname = "log";
+  var files = DriveApp.getFilesByName(fname);
+  var doc = (files.hasNext()) ? DocumentApp.openById(files.next().getId()) : DocumentApp.create(fname);
+  doc.getBody().editAsText().appendText(Logger.getLog());
 }
 
 function emailHtml(html, images) {
@@ -41,29 +47,12 @@ function emailHtml(html, images) {
    });
 }
 
-function createDocumentForHtml(html, images) {
-  var name = DocumentApp.getActiveDocument().getName()+".html";
-  var newDoc = DocumentApp.create(name);
-  newDoc.getBody().setText(html);
-  for(var j=0; j < images.length; j++)
-    newDoc.getBody().appendImage(images[j].blob);
-  newDoc.saveAndClose();
-}
-
-function dumpAttributes(atts) {
-  // Log the paragraph attributes.
-  for (var att in atts) {
-    Logger.log(att + ":" + atts[att]);
-  }
-}
-
 function processItem(item, listCounters, images) {
   var output = [];
   var prefix = "", suffix = "";
-
+  
   if (item.getType() == DocumentApp.ElementType.PARAGRAPH) {
     switch (item.getHeading()) {
-        // Add a # for each heading level. No break, so we accumulate the right number.
       case DocumentApp.ParagraphHeading.HEADING6: 
         prefix = "<h6>", suffix = "</h6>"; break;
       case DocumentApp.ParagraphHeading.HEADING5: 
@@ -79,31 +68,24 @@ function processItem(item, listCounters, images) {
       default: 
         prefix = "<p>", suffix = "</p>";
     }
-
     if (item.getNumChildren() == 0)
       return "";
   }
-  else if (item.getType() == DocumentApp.ElementType.INLINE_IMAGE)
-  {
+  else if (item.getType() == DocumentApp.ElementType.INLINE_IMAGE) {
     processImage(item, images, output);
   }
   else if (item.getType()===DocumentApp.ElementType.LIST_ITEM) {
-    var listItem = item;
-    var gt = listItem.getGlyphType();
-    var key = listItem.getListId() + '.' + listItem.getNestingLevel();
+    var gt = item.getGlyphType();
+    var key = item.getListId() + '.' + item.getNestingLevel();
     var counter = listCounters[key] || 0;
 
     // First list item
     if ( counter == 0 ) {
       // Bullet list (<ul>):
-      if (gt === DocumentApp.GlyphType.BULLET
-          || gt === DocumentApp.GlyphType.HOLLOW_BULLET
-          || gt === DocumentApp.GlyphType.SQUARE_BULLET) {
-        prefix = '<ul><li>', suffix = "</li>";
-
-          suffix += "</ul>";
-        }
-      else {
+      if (gt === DocumentApp.GlyphType.BULLET || gt === DocumentApp.GlyphType.HOLLOW_BULLET || gt === DocumentApp.GlyphType.SQUARE_BULLET) {
+        prefix = '<ul><li>';
+        suffix = "</li>";
+      } else {
         // Ordered list (<ol>):
         prefix = "<ol><li>", suffix = "</li>";
       }
@@ -114,16 +96,13 @@ function processItem(item, listCounters, images) {
     }
 
     if (item.isAtDocumentEnd() || (item.getNextSibling() && (item.getNextSibling().getType() != DocumentApp.ElementType.LIST_ITEM))) {
-      if (gt === DocumentApp.GlyphType.BULLET
-          || gt === DocumentApp.GlyphType.HOLLOW_BULLET
-          || gt === DocumentApp.GlyphType.SQUARE_BULLET) {
+      if (gt === DocumentApp.GlyphType.BULLET || gt === DocumentApp.GlyphType.HOLLOW_BULLET || gt === DocumentApp.GlyphType.SQUARE_BULLET) {
         suffix += "</ul>";
       }
       else {
         // Ordered list (<ol>):
         suffix += "</ol>";
       }
-
     }
 
     counter++;
@@ -133,97 +112,74 @@ function processItem(item, listCounters, images) {
   output.push(prefix);
 
   if (item.getType() == DocumentApp.ElementType.TEXT) {
-    processText(item, output);
+    processTextItem(item, output);
   }
   else {
-
-
-    if (item.getNumChildren) {
-      var numChildren = item.getNumChildren();
-
-      // Walk through all the child elements of the doc.
-      for (var i = 0; i < numChildren; i++) {
-        var child = item.getChild(i);
-        output.push(processItem(child, listCounters, images));
+    if (item.getNumChildren()) {
+      for (var i = 0; i < item.getNumChildren(); i++) {
+        output.push(processItem(item.getChild(i), listCounters, images));
       }
     }
-
   }
 
   output.push(suffix);
   return output.join('');
 }
 
-
-function processText(item, output) {
+function processTextItem(item, output) {
+  sanitizeTextItem(item);
+  
   var text = item.getText();
   var indices = item.getTextAttributeIndices();
-
-  if (indices.length <= 1) {
-    // Assuming that a whole para fully italic is a quote
-    if(item.isBold()) {
-      output.push('<strong>' + text + '</strong>');
-    }
-    else if(item.isItalic()) {
-      output.push('<blockquote>' + text + '</blockquote>');
-    }
-    else if (text.trim().indexOf('http://') == 0) {
-      output.push('<a href="' + text + '" rel="nofollow">' + text + '</a>');
-    }
-    else {
-      output.push(text);
-    }
-  }
-  else {
-
-    for (var i=0; i < indices.length; i ++) {
+  
+  if(indices.length > 1) {
+    for (var i=0; i<indices.length; i++) {
       var partAtts = item.getAttributes(indices[i]);
       var startPos = indices[i];
       var endPos = i+1 < indices.length ? indices[i+1]: text.length;
       var partText = text.substring(startPos, endPos);
-
-      Logger.log(partText);
-
-      if (partAtts.ITALIC) {
-        output.push('<i>');
-      }
-      if (partAtts.BOLD) {
-        output.push('<strong>');
-      }
-      if (partAtts.UNDERLINE) {
-        output.push('<u>');
-      }
-
-      // If someone has written [xxx] and made this whole text some special font, like superscript
-      // then treat it as a reference and make it superscript.
-      // Unfortunately in Google Docs, there's no way to detect superscript
-      if (partText.indexOf('[')==0 && partText[partText.length-1] == ']') {
-        output.push('<sup>' + partText + '</sup>');
-      }
-      else if (partText.trim().indexOf('http://') == 0) {
-        output.push('<a href="' + partText + '" rel="nofollow">' + partText + '</a>');
-      }
-      else {
-        output.push(partText);
-      }
-
-      if (partAtts.ITALIC) {
-        output.push('</i>');
-      }
-      if (partAtts.BOLD) {
-        output.push('</strong>');
-      }
-      if (partAtts.UNDERLINE) {
-        output.push('</u>');
-      }
-
+      processText(partText, partAtts, output);
     }
+  }
+  else {
+    processText(text, item.getAttributes(), output);
   }
 }
 
+function sanitizeTextItem(item) {
+  // Sanitize text from flickr garbage
+  item.replaceText('<script.*/script>', '');
+  item.replaceText('data-flickr-embed="true" ', 'target="_blank"');
+  item.replaceText('(<img)', '<img class="aligncenter"');  
+}
 
-function processImage(item, images, output)
-{
+function processText(text, attr, output) {
+  var prefix = "", suffix = "";
+  
+  Logger.log(text);
+  for(var att in attr) {
+    Logger.log(att + " : " + attr[att]);
+  }
+  
+  // Possible attribute keys:
+  // FONT_SIZE : ITALIC : STRIKETHROUGH : FOREGROUND_COLOR : BOLD : LINK_URL : UNDERLINE : FONT_FAMILY : BACKGROUND_COLOR
+  if(attr['BOLD']) {
+    prefix += "<strong>", suffix = "</strong>" + suffix;
+  }
+  if(attr['ITALIC']) {
+    prefix += "<i>", suffix = "</i>" + suffix;
+  }
+  if(attr['UNDERLINE'] && !attr['LINK_URL']) {
+    prefix += "<u>", suffix = "</u>" + suffix;
+  }
+  if(attr['LINK_URL']) {
+    prefix += "<a href=" + attr['LINK_URL'] + " target=\"_blank\">", suffix = "</a>" + suffix;
+  }
+  
+  output.push(prefix + text + suffix);
+}
+
+function processImage(item, images, output) {
   images = images || [];
   var blob = item.getBlob();
   var contentType = blob.getContentType();
